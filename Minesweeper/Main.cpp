0e1671f8838e2b4065f8936f7c3dae5a312efce2
Miro Šimić <34944272+MiroTheMighty@users.noreply.github.com>
2024-01-01 21:01:27 +0100

    Delete Minesweeper directory

--- Minesweeper/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include"SFML/Graphics.hpp"
-#include"Menu.h"
-
-int main() {
-	sf::RenderWindow window(sf::VideoMode(400, 270),"Menu");
-	drawMainMenu(window);
-	
-	return 0;
-}
--- Minesweeper/Menu.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-std::vector<std::pair<std::string, int>> readHighscores();
-bool drawHighscoreMenu(sf::RenderWindow& window);
-void drawMainMenu(sf::RenderWindow& window);
